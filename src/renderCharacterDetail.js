77875260ec41e6d5438d47589e0390521e334287
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-19 12:50:27 +0200

    adding function for render Character detail

---
+++ src/renderCharacterDetail.js
@@ -0,0 +1,30 @@
+/** @format */
+
+import createHtmlElement from './helpers/createHtmlElement.js';
+import createImgElement from './helpers/createImgElement.js';
+import renderDialog from './renderDialog.js';
+
+export default function (index, renderData, mainCharactersDetails) {
+	// remove renderData[index];
+	const detailContainer = createHtmlElement('div');
+	const name = createHtmlElement('div', `Imie: ${renderData[index].name}`);
+	const gender = createHtmlElement('div', `Płeć: ${renderData[index].gender}`);
+	const status = createHtmlElement(
+		'div',
+		`Status: ${renderData[index].status}`
+	);
+	const image = createImgElement(
+		'img',
+		renderData[index].image,
+		renderData[index].name
+	);
+
+	detailContainer.classList.add('main-characters-details-container');
+
+	detailContainer.append(name, gender, status, image);
+	mainCharactersDetails.append(detailContainer);
+
+	image.addEventListener('click', function () {
+		renderDialog(index, renderData);
+	});
+}
