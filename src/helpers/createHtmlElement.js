85d8155f5f8759e3a0aa951d294ebb155832b31f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-19 21:58:01 +0200

    adding condition for adding text to html element

--- src/helpers/createHtmlElement.js
+++ src/helpers/createHtmlElement.js
@@ -2,6 +2,8 @@
 
 export default function createHtmlElement(htmlTag, text = '') {
 	const element = document.createElement(htmlTag);
-	element.textContent = text;
+	if (text !== '') {
+		element.textContent = text;
+	}
 	return element;
 }
