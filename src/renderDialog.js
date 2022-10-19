abf0a9f1b8e01c94b0cc6ae28caedf900d295be9
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-19 12:49:36 +0200

    adding function for render dialog

---
+++ src/renderDialog.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import createHtmlElement from './helpers/createHtmlElement.js';
+import createImgElement from './helpers/createImgElement.js';
+
+export default function (index, renderData) {
+	// creating elements and new image for dialog to prevent removing img in character detail container
+	const dialog = createHtmlElement('dialog');
+	const dialogImg = createImgElement(
+		'img',
+		renderData[index].image,
+		renderData[index].name
+	);
+	const closeBtn = createHtmlElement('button', 'Zamknij');
+
+	//appending element to DOM
+	dialog.append(dialogImg, closeBtn);
+	document.body.append(dialog);
+
+	dialog.showModal();
+
+	//event for closing dialog window
+	closeBtn.addEventListener('click', function () {
+		dialog.close();
+	});
+}
